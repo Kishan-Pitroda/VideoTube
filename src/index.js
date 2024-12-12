982a6dc191c19e131756f49d6a83524972ffd8d6
Kishan-Pitroda <kishanpitroda111.kp@gmail.com>
2024-12-12 19:05:15 +0530

    Added utilities

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import { configDotenv } from "dotenv";
+import express from "express";
 import connectDB from "./db/index.js";
+
+const app = express();
 
 configDotenv({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error", error);
+  });
+
+export { app };
