9f29d90549c94b744ae1b51b1e4ba083d11ab7a5
Kishan-Pitroda <kishanpitroda111.kp@gmail.com>
2024-12-24 12:01:12 +0530

    fix user controllers typo error

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -338,7 +338,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     },
     {
       $lookup: {
-        from: "subscription",
+        from: "subscriptions",
         localField: "_id",
         foreignField: "channel",
         as: "subscribers",
@@ -346,7 +346,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
     },
     {
       $lookup: {
-        from: "subscription",
+        from: "subscriptions",
         localField: "_id",
         foreignField: "subscriber",
         as: "subscribedTo",
